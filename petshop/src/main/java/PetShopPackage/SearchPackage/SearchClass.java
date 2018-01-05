29ca4db4937c963ad5e8eec797703bfd17a46d5c
Makarkin <34379047+Makarkin@users.noreply.github.com>
2018-01-05 16:49:00 +0400

    Update SearchClass.java

--- petshop/src/main/java/PetShopPackage/SearchPackage/SearchClass.java
+++ petshop/src/main/java/PetShopPackage/SearchPackage/SearchClass.java
@@ -7,50 +7,12 @@ import java.util.List;
 
 public class SearchClass {
 
-    private List<Animal> search_res;
-
-    public void searchName(String name) {
+      public void search(int cost, String...input_strings) {
         List<Animal> search_tmp = HolderClass.getHolder_storage().getStorage();
-        for (Animal a : search_tmp) {
-            if (name.equals(a.getName())) {
-                search_res.add(a);
-            }
-        }
-
-        System.out.println("Results of search by name: +/n" + search_res.toString());
-    }
-
-    public void searchCharacter(String name) {
-        List<Animal> search_tmp = HolderClass.getHolder_storage().getStorage();
-        for (Animal a : search_tmp) {
-            if (name.equals(a.getCharacter())) {
-                search_res.add(a);
-            }
-        }
-
-        System.out.println("Results of search by character: +/n" + search_res.toString());
-    }
-
-    public void searchCost(int cost) {
-        List<Animal> search_tmp = HolderClass.getHolder_storage().getStorage();
-        for (Animal a : search_tmp) {
-            if (cost == (a.getCost())) {
-                search_res.add(a);
-            }
-        }
-
-        System.out.println("Results of search by cost: +/n" + search_res.toString());
-    }
-
-    public void Search(String breedName) {
-        List<Animal> search_tmp = HolderClass.getHolder_storage().getStorage();
-        for (Animal a : search_tmp) {
-            if (breedName == (a.getBreed())) {
-                search_res.add(a);
-            }
-        }
-
-        System.out.println("Results of search by breed: +/n" + search_res.toString());
+        search_tmp.stream().filter(s-> Arrays.asList(input_strings).contains(s.getName()) |
+                Arrays.asList(input_strings).contains(s.getBreed()) |
+                Arrays.asList(input_strings).contains(s.getCharacter()) &&
+                s.getCost() <= cost).forEach(s -> System.out.println("Results of search: " + s));
     }
 
     public SearchClass() {
