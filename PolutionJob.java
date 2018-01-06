b37a240457d5e54223c8761c06da224d1e4b8b6a
Makarkin <34379047+Makarkin@users.noreply.github.com>
2018-01-06 13:57:28 +0400

    Add files via upload

---
+++ PolutionJob.java
@@ -0,0 +1,22 @@
+package petshop.cleanPollution;
+
+import petshop.animals.Animal;
+import petshop.holder.HolderClass;
+
+public class PolutionJob extends Thread {
+
+    @Override
+    public void run() {
+        int key;
+        try {
+            while (true) {
+                key = (int) (Math.random() * 11) + 20;
+                Thread.sleep(50);
+                for (Animal a : HolderClass.getHolderStorage().getAnimals()) {
+                    a.setDegreeOfPollution(key);
+                }
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+}
