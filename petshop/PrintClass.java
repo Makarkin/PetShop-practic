f4e160f950d99af85c31709b19c7428cf7b292fa
Makarkin <34379047+Makarkin@users.noreply.github.com>
2018-01-06 14:02:10 +0400

    Add files via upload

---
+++ petshop/PrintClass.java
@@ -0,0 +1,14 @@
+package petshop.sellBuyPrint;
+
+import petshop.animals.Animal;
+
+public class PrintClass {
+
+    public void printForOperations(Animal animal, String operationName) {
+        if ("sell".equals(operationName)) {
+            System.out.printf("Shop sold an %s. Its breed is %s\n", animal.getName().toString(), animal.getBreed());
+        } else if ("buy".equals(operationName)) {
+            System.out.printf("Shop bought an  %s. Its breed is %s\n", animal.getName().toString(), animal.getBreed());
+        }
+    }
+}
